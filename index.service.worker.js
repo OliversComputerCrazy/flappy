d7221baa7c59da063257e4194f63033f153eb6b2
oliver <Oliverjohnstone10@gmail.com>
2026-02-17 14:30:10 +0000

    Added a scroll container in the skins menu and a new skin pack

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1759771868|3368098817';
+const CACHE_VERSION = '1771338256|2060728669';
 /** @type {string} */
 const CACHE_PREFIX = 'flappy-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
